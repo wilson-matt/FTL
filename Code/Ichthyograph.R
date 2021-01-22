edfbbcc5719fbbfc2650423314af999fe6411f3f
Liz Perkin <liz@Lizs-Air.hitronhub.home>
2021-01-21 21:11:11 -0700

    Added regular scatterplot code

--- Code/Ichthyograph.R
+++ Code/Ichthyograph.R
@@ -7,8 +7,10 @@ library(misc3d)
 library(openxlsx)
 library(readxl)
 library(tidyverse)
+library(ggplot2)
+library(viridis)
 
-ichthy <- read_excel(("FTL/Data/HypotheticalIchthyograph.xlsx"), 
+ichthy <- read_excel(("Data/HypotheticalIchthyograph.xlsx"), 
                      sheet = "FTL")
 
 # x, y and z coordinates
@@ -36,3 +38,22 @@ scatter3D(x, y, z, pch = 16, cex = 1, theta = 45, #theta for rotating to best vi
               addlines = TRUE, length = 0.5, width = 0.5,
               labels = c("Smolt", "Fry", "Adult")) )
 
+
+#Scatter plot: Flow ---------------------------------------------------------------
+flow <- ggplot(ichthy,aes(x = Discharge,
+                              y = Abundance,
+                              fill=LifePhase)) +
+  geom_point(size = 5,shape = 21, colour = "black",position=position_dodge2(0.2)) +
+  scale_colour_viridis_c()+
+  ylab(expression(paste(bold("Fish Abundance")))) +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        axis.title = element_text(face = "bold"))
+#axis.text.x = element_text(angle=90))
+
+flow+labs(fill="LifePhase")
+
+ggsave(file=paste0("Figures/","Flow Ichthyograph",
+                   ".png"), 
+       flow+labs(fill="LifePhase"), width = 6.5, 
+       height = 4.5, units = "in", dpi=300)
